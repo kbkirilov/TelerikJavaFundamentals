9bf69c21b6e5bc2c5d202920db5d900eab6f8a47
Owner <kirilovmail90@gmail.com>
2024-04-26 13:44:18 +0300

    adding test strings

--- src/Chapter4DataTasks1HelloTelerikAcademy.java
+++ src/Chapter4DataTasks1HelloTelerikAcademy.java
@@ -4,6 +4,6 @@ public class Chapter4DataTasks1HelloTelerikAcademy {
 
         String test = "TEST";
         String test2 = "Test2";
-        String test3 = "Test3";
+        String test3 = "Test";
     }
 }
