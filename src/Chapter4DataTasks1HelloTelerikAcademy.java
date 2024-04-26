2855bf04078e89ee2943d3c58ea233066f51c440
Owner <kirilovmail90@gmail.com>
2024-04-26 13:36:29 +0300

    first commit

--- src/Chapter4DataTasks1HelloTelerikAcademy.java
+++ src/Chapter4DataTasks1HelloTelerikAcademy.java
@@ -1,5 +1,7 @@
 public class Chapter4DataTasks1HelloTelerikAcademy {
     public static void main(String[] args) {
         System.out.println("Hello, Telerik Academy!");
+
+        String test = "TEST";
     }
 }
