0265b781bd7a1192958f860659eb9a98ceb71dd2
Owner <kirilovmail90@gmail.com>
2024-05-10 12:48:54 +0300

    a second solution without using Array list

--- src/Chapter10_Arrays practice/Pack3Task2StrangeOrders.java
+++ src/Chapter10_Arrays practice/Pack3Task2StrangeOrders.java
@@ -22,72 +22,51 @@ public class Pack3Task2StrangeOrders {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Reading the numbers from the console as string
-        String[] numbersStr = scanner.nextLine().split(",");
+        // Get numbersAsString as array
+        String[] numbersAsString  = scanner.nextLine().split(",");
 
-        // Declaring a new array to store the parsed numbers
-        int[] numbers = new int[numbersStr.length];
-
-        // Filling the "numbers" array
+        // Convert the string array to a numbers array
+        int[] numbers = new int[numbersAsString .length];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Integer.parseInt(numbersStr[i]);
+            numbers[i] = Integer.parseInt(numbersAsString [i]);
         }
 
 
-        // Using array list to solve the task
-        ArrayList<Integer> arrListNegatives = new ArrayList<>();
-        ArrayList<Integer> arrListZeros = new ArrayList<>();
-        ArrayList<Integer> arrListPositive = new ArrayList<>();
+        // Create a new string array in which we will keep the 'strange order'
+        String[] result = new String[numbers.length];
 
 
+        // This will point where to write in the 'result' array
+        int index = 0;
 
-        // Finding out the negative numbers
+        // This loop is for all the negative number
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < 0) {
-                arrListNegatives.add(numbers[i]);
+                result[index] = String.valueOf(numbers[i]);
+                index++;
             }
         }
 
-        // Finding out the zero numbers
+        // This loop is for all zeroes. The index is not
+        // reset because it needs to write values to the
+        // correct places
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) {
-                arrListZeros.add(numbers[i]);
+                result[index] = String.valueOf(numbers[i]);
+                index++;
             }
         }
 
-        // Finding out the positive numbers
+        // This loop is for all the positive numbers
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > 0) {
-                arrListPositive.add(numbers[i]);
+                result[index] = String.valueOf(numbers[i]);
+                index++;
             }
         }
 
+        System.out.print(String.join(",", result));
 
-        // This will store the ordered array elements
-        ArrayList<Integer> arrListCombined = new ArrayList<>();
-
-
-        // Adding all the values from the previous three arrays
-        // to the combined array
-        arrListCombined.addAll(arrListNegatives);
-        arrListCombined.addAll(arrListZeros);
-        arrListCombined.addAll(arrListPositive);
-
-
-        // Printing the result according to the tas requirements
-        for (int i = 0; i < arrListCombined.size(); i++) {
-            System.out.print(arrListCombined.get(i));
-
-            // When we get to the last array element the program won't
-            // print ","
-            if (i < arrListCombined.size() - 1) {
-                System.out.print(",");
-            }
-        }
-
-
-        // New line after the result has been printed
-        System.out.println();
 
     }
 }
