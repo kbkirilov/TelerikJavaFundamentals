0fe7047dfcbccc316cd8cef59ba5492ba75d41e5
Owner <kirilovmail90@gmail.com>
2024-05-17 08:47:52 +0300

    fixed the code for printing the right output

--- src/MockExams1/MockExams1Task2PrimeTriangle.java
+++ src/MockExams1/MockExams1Task2PrimeTriangle.java
@@ -18,6 +18,7 @@ Each row contains all the numbers for 1 to PRIME_NUMBER
  */
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MockExams1Task2PrimeTriangle {
@@ -27,33 +28,28 @@ public class MockExams1Task2PrimeTriangle {
         // Reading n
         int n = scanner.nextInt();
 
-        // Generate a sequence of 1 to N inclusive
         int[] numbers = new int[n];
 
         // Used to assign elements in the array numbers
         int index = 1;
 
-        //
-        StringBuilder arrPrimesAsStringBuilder = new StringBuilder();
 
-        // Used to store how many prime number we have found in the array
-        // Will be used in the loop in order to print the correct lines
-        int primesCount = 0;
+        // Array list to store all the prime numbers from the input
+        // once they have been filtered
+        ArrayList<Integer> arrPrimesArrayList = new ArrayList<>();
 
         // Current number
         int currNumber = 0;
 
-
-        // Filling the array with the index variable
+        // Filling the array with the elements from 1 to N
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = index;
             index++;
         }
 
-        // Check is a number is prime or not
+        // Check if a number is prime or not
         for (int i = 0; i < numbers.length; i++) {
 
-            // Is prime?
             boolean isPrime = true;
 
             // The current number from the array at index i
@@ -69,30 +65,39 @@ public class MockExams1Task2PrimeTriangle {
                 }
             }
 
-            // Add the count of prime numbers
+            // Add the prime numbers as elements to the array list
             if (isPrime) {
-                primesCount++;
-                arrPrimesAsStringBuilder.append(currNumber);
+                arrPrimesArrayList.add(currNumber);
             }
-
 
         }
 
-
-        String[] arrPrimesAsString = arrPrimesAsStringBuilder.toString().split("");
-
-        int[] arrPrimes = new int[arrPrimesAsString.length];
+        // Convert the array list (which now contains only prime numbers from 1
+        // to N) to an array.
+        Integer[] arrPrimes = arrPrimesArrayList.toArray(new Integer[0]);
 
         boolean arePrimes = true;
 
-        for (int i = 0; i < arrPrimes.length; i++) {
-            arrPrimes[i] = Integer.parseInt(arrPrimesAsString[i]);
-        }
-
+        // Printing
         for (int i = 0; i < arrPrimes.length; i++) {
             for (int j = 1; j <= arrPrimes[i]; j++) {
-                System.out.print(j);
+
+                arePrimes = true;
+
+                // Check if number is divisible by any integer from 2 to its square root
+                for (int k = 2; k <= Math.sqrt(j) ; k++) {
+                    if (j % k == 0) {
+                        arePrimes = false;
+                    }
+                }
+
+                if (arePrimes) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
             }
+
             System.out.println();
         }
     }
